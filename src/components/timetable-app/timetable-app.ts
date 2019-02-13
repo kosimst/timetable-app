4c80fff185c4ae6b083ba46f5636ba71524df42c
simon <simon@pop-os.localdomain>
2019-02-13 18:58:41 +0100

    Restrucured app

--- src/components/timetable-app/timetable-app.ts
+++ src/components/timetable-app/timetable-app.ts
@@ -14,16 +14,21 @@ import { installOfflineWatcher } from 'pwa-helpers/network.js'
 import { installRouter } from 'pwa-helpers/router.js'
 import { updateMetadata } from 'pwa-helpers/metadata.js'
 
-import { store, RootState } from '../store.js'
+import 'boxicons'
+
+import { store, RootState } from '../../store.js'
 
 import {
   navigate,
   updateOffline,
   updateDrawerState,
   AppActionUpdateDrawerState,
-} from '../actions/app.js'
+} from '../../actions/app.js'
 
-import './snack-bar.js'
+import '../snack-bar.js'
+
+// Imports os styles
+import sidebarStyles from './navbar-styles.js'
 
 class TimetableApp extends connect(store)(LitElement) {
   @property({ type: String })
@@ -42,44 +47,35 @@ class TimetableApp extends connect(store)(LitElement) {
     :host {
       display: block;
 
-      --app-drawer-width: 256px;
+      /* Theme */
+      --theme-sidebar-color: #172a3a;
 
-      --app-primary-color: #e91e63;
-      --app-secondary-color: #293237;
-      --app-dark-text-color: var(--app-secondary-color);
-      --app-light-text-color: white;
-      --app-section-even-color: #f7f7f7;
-      --app-section-odd-color: white;
-
-      --app-header-background-color: white;
-      --app-header-text-color: var(--app-dark-text-color);
-      --app-header-selected-color: var(--app-primary-color);
-
-      --app-drawer-background-color: var(--app-secondary-color);
-      --app-drawer-text-color: var(--app-light-text-color);
-      --app-drawer-selected-color: #78909c;
+      /* Layout */
+      --app-sidebar-width: 64px;
     }
 
     /* Styling of sidebar */
-    #sidebar {
-      position: fixed;
-      left: 0;
-      top: 0;
-
-      width: 80px;
-      height: 100vh;
-
-      background: #172a3a;
-    }
+    ${sidebarStyles}
   `
 
   protected render(): TemplateResult {
     return html`
       <aside id="sidebar">
         <nav>
-          <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
-          <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
-          <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
+          <a ?selected="${this._page === 'view1'}" href="/view1">
+            <box-icon type="solid" name="user-detail"></box-icon>
+          </a>
+          <div class="center">
+            <a ?selected="${this._page === 'view2'}" href="/view2"
+              ><box-icon name="calendar"></box-icon
+            ></a>
+            <a ?selected="${this._page === 'view3'}" href="/view3"
+              ><box-icon name="book-open" type="solid"></box-icon
+            ></a>
+          </div>
+          <a ?selected="${this._page === 'view3'}" href="/view3"
+            ><box-icon name="cog"></box-icon
+          ></a>
         </nav>
       </aside>
 
@@ -137,7 +133,6 @@ class TimetableApp extends connect(store)(LitElement) {
     this._page = state.app!.page
     this._offline = state.app!.offline
     this._snackbarOpened = state.app!.snackbarOpened
-    this._drawerOpened = state.app!.drawerOpened
   }
 }
 
