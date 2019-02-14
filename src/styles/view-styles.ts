ee665d80d0f5876eb64a440abb6420b13f855f68
Simon Steindl <kosimst@gmail.com>
2019-02-14 12:33:59 +0100

    Added grid

--- src/styles/view-styles.ts
+++ src/styles/view-styles.ts
@@ -1,3 +1,21 @@
 import { css, CSSResult } from 'lit-element'
 
-export const styles: CSSResult = css``
+export const styles: CSSResult = css`
+  :host {
+    display: block;
+
+    width: 100%;
+    height: 100%;
+  }
+
+  h1#title {
+    color: white;
+
+    margin: 0;
+    height: fit-content;
+
+    font-size: 72px;
+    font-weight: 500;
+    font-family: Pacifico;
+  }
+`
