809d5c3b299ecf6a1cdbb4053d82f936a5686015
simon <simon@pop-os.localdomain>
2019-02-19 17:04:56 +0100

    Add timetable-grid component

---
+++ src/components/timetable-grid/timetable-grid.ts
@@ -0,0 +1,11 @@
+import { html, css, LitElement } from 'lit-element'
+
+class TimetableGrid extends LitElement {
+  protected render() {
+    return html`
+    
+    `
+  }
+}
+
+customElements.define('timetable-grid', TimetableGrid)
