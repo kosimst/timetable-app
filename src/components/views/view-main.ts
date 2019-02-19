47a37ce491153b692d3392a3a632d9622ea9c11c
simon <simon@pop-os.localdomain>
2019-02-19 18:40:43 +0100

    Added menu bar

--- src/components/views/view-main.ts
+++ src/components/views/view-main.ts
@@ -8,12 +8,15 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { html, css } from 'lit-element'
+import { html, css, CSSResult } from 'lit-element'
 import { PageViewElement } from '../page-view-element.js'
 
 // These are the shared styles needed by this element.
 import { styles as SharedStyles } from '../styles/shared-styles.js'
 import { styles as ViewStyles } from '../styles/view-styles.js'
+
+import '../timetable-grid/timetable-grid.js'
+import '../timetable-toggle/timetable-toggle.js'
 
 class ViewMain extends PageViewElement {
   static styles: CSSResult = css`
@@ -23,23 +26,32 @@ class ViewMain extends PageViewElement {
     :host {
       display: grid;
 
-      grid-template-columns: 90% 10%;
+      grid-template-columns: 2% 90% 8%;
       grid-template-rows: 15% 10% 80%;
       grid-column-gap: 0px;
       grid-row-gap: 0px;
+
       justify-items: stretch;
       align-items: stretch;
     }
 
     h1#title {
-      padding-left: 64px;
       align-self: end;
+      grid-column-start: 2;
+    }
+
+    #menubar {
+      grid-column-start: 2;
+      align-self: center;
     }
   `
 
   protected render() {
     return html`
       <h1 id="title">Mein Stundenplan</h1>
+      <div id="menubar" role="menubar">
+        <timetable-toggle on="Klassen" off="Lehrer"></timetable-toggle>
+      </div>
     `
   }
 }
