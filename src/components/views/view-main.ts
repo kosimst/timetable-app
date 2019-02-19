3ab59b3b6a5c5c3597a686484a49ac5410e28e36
simon <simon@pop-os.localdomain>
2019-02-19 21:23:39 +0100

    Migrated views

--- src/components/views/view-main.ts
+++ src/components/views/view-main.ts
@@ -16,61 +16,17 @@ import { styles as SharedStyles } from '../styles/shared-styles.js'
 import { styles as ViewStyles } from '../styles/view-styles.js'
 
 import '../timetable-grid/timetable-grid.js'
-import '../timetable-toggle/timetable-toggle.js'
-import '../timetable-select/timetable-select.js'
 
 class ViewMain extends PageViewElement {
   static styles: CSSResult = css`
     ${SharedStyles}
-    ${ViewStyles}
-
-    :host {
-      display: grid;
-
-      grid-template-columns: 2% 90% 8%;
-      grid-template-rows: 12% 10% 83%;
-      grid-column-gap: 0px;
-      grid-row-gap: 0px;
-
-      justify-items: stretch;
-      align-items: stretch;
-    }
-
-    h1#title {
-      align-self: end;
-      grid-column-start: 2;
-    }
-
-    #menubar {
-      grid-column-start: 2;
-      align-self: center;
-
-      height: 40px;
-    }
-
-    #menubar > * {
-      display: inline-block;
-
-      float: left;
-
-      margin-right: 32px;
-    }
-  `
+    ${ViewStyles}`
 
   protected render() {
-    return html`
-      <h1 id="title">Mein Stundenplan</h1>
-      <div id="menubar" role="menubar">
-        <timetable-toggle on="Klassen" off="Lehrer"></timetable-toggle>
-        <timetable-select>
-          <option value="1A">1A</option>
-          <option value="1B">1B</option>
-        </timetable-select>
-      </div>
-    `
+    return html``
   }
 }
 
-export const title: string = 'Mein Stundenplan'
+export const title: string = 'Meine Startseite'
 
 customElements.define('view-main', ViewMain)
---
+++ src/components/views/view-timetable.ts
@@ -0,0 +1,80 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+import { html, css, CSSResult } from 'lit-element'
+import { PageViewElement } from '../page-view-element.js'
+
+// These are the shared styles needed by this element.
+import { styles as SharedStyles } from '../styles/shared-styles.js'
+import { styles as ViewStyles } from '../styles/view-styles.js'
+
+import '../timetable-grid/timetable-grid.js'
+import '../timetable-toggle/timetable-toggle.js'
+import '../timetable-select/timetable-select.js'
+
+class ViewTimetable extends PageViewElement {
+  static styles: CSSResult = css`
+    ${SharedStyles}
+    ${ViewStyles}
+
+    :host {
+      display: grid;
+
+      grid-template-columns: 2% 90% 8%;
+      grid-template-rows: 12% 10% 83%;
+      grid-column-gap: 0px;
+      grid-row-gap: 0px;
+
+      justify-items: stretch;
+      align-items: stretch;
+    }
+
+    h1#title {
+      align-self: end;
+      grid-column-start: 2;
+    }
+
+    #menubar {
+      grid-column-start: 2;
+      align-self: center;
+
+      height: 40px;
+    }
+
+    #menubar > * {
+      display: inline-block;
+
+      float: left;
+
+      margin-right: 32px;
+    }
+  `
+
+  protected render() {
+    return html`
+      <h1 id="title">Mein Stundenplan</h1>
+      <div id="menubar" role="menubar">
+        <timetable-toggle on="Klassen" off="Lehrer"></timetable-toggle>
+        <timetable-select>
+          <option value="1A">1A</option>
+          <option value="1B">1B</option>
+          <option value="2A">2A</option>
+          <option value="2B">2B</option>
+          <option value="3A">3A</option>
+          <option value="3B">3B</option>
+        </timetable-select>
+      </div>
+    `
+  }
+}
+
+export const title: string = 'Mein Stundenplan'
+
+customElements.define('view-timetable', ViewTimetable)
