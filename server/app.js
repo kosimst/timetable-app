e9c55a0f43fabeb41b16aa0054d890d76e0e9c4c
Simon Steindl <kosimst@gmail.com>
2019-02-20 10:06:14 +0100

    Added server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const prpl = require('prpl-server')
+const rendertron = require('rendertron-middleware')
+
+const app = express()
+
+/* Redirect API calls to GraphQL */
+
+/* Rendertron proxy for bots */
+app.use(rendertron.makeMiddleware({
+  proxyUrl: ' https://render-tron.appspot.com/render'
+}))
+
+app.get('/*', prpl.makeHandler('./build'))
+
+app.listen(process.env.PORT || 3000)
