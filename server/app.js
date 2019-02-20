8c8e968846a6062d2ca5aaeef7f59dcba4e6f991
Simon Steindl <kosimst@gmail.com>
2019-02-20 10:15:16 +0100

    Registered builds

--- server/app.js
+++ server/app.js
@@ -11,6 +11,20 @@ app.use(rendertron.makeMiddleware({
   proxyUrl: 'https://render-tron.appspot.com/render'
 }))
 
-app.get('/*', prpl.makeHandler('./build'))
+app.get('/*', prpl.makeHandler('./build', {
+  builds: [{
+    name: 'esm-unbundled',
+    browserCapabilities: ['push', 'es2018', 'modules']
+  }, {
+    name: 'esm-bundled',
+    browserCapabilities: ['es2018', 'modules']
+  }, {
+    name: 'es6-bundled',
+    browserCapabilities: ['es2015']
+  }, {
+    name: 'es5-bundled',
+    browserCapabilities: []
+  }],
+}))
 
 app.listen(process.env.PORT || 3000)
