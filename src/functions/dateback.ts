13279d4a0eb98cf9d6aa2131da736e26138cba20
simon <simon@pop-os.localdomain>
2019-03-18 15:44:02 +0100

    Added dateback function

---
+++ src/functions/dateback.ts
@@ -0,0 +1,14 @@
+export default (date: Date) => {
+  return `${
+    date.setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0)
+      ? 'heute'
+      : date.setHours(0, 0, 0, 0) ===
+        new Date(Date.now() - 24 * 60 * 60 * 1000).setHours(0, 0, 0, 0)
+      ? 'gestern'
+      : `vor ${Math.trunc(
+          (Date.now() - date.getTime()) / 1000 / 60 / 60 / 24,
+        )} Tagen`
+  } um ${String(date.getHours()).padStart(2, '0')}:${String(
+    date.getMinutes(),
+  ).padStart(2, '0')}`
+}
