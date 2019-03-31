83b26e4c6e080871cb653e29d2a3732e1e66693c
simon <simon@pop-os.localdomain>
2019-03-31 19:11:39 +0200

    Added timetable grid layout

--- src/components/timetable-grid/timetable-grid.ts
+++ src/components/timetable-grid/timetable-grid.ts
@@ -1,10 +1,41 @@
-import { html, LitElement, css } from 'lit-element'
+import { html, LitElement, css, unsafeCSS as cssLiteral } from 'lit-element'
 
 class TimetableGrid extends LitElement {
   static styles = css`
     :host {
       display: grid;
+
+      grid-template-columns: repeat(5, 1fr);
+      grid-auto-rows: 80px;
+
+      grid-gap: 12px;
     }
+
+    /* Align per day */
+    ${cssLiteral(
+      [...Array(5)]
+        .map(
+          (_, i) => css`
+          ::slotted(timetable-hour[day="${cssLiteral(i)}"]) {
+            grid-column-start: ${cssLiteral(i + 1)};
+          }
+        `,
+        )
+        .join(' '),
+    )}
+
+    /* Align per hour */
+    ${cssLiteral(
+      [...Array(5)]
+        .map(
+          (_, i) => css`
+          ::slotted(timetable-hour[hour="${cssLiteral(i)}"]) {
+            grid-row-start: ${cssLiteral(i + 1)};
+          }
+        `,
+        )
+        .join(' '),
+    )}
   `
 
   protected render() {
