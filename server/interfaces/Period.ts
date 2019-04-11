f94a93b6034f753c44b721e1d13003d62358781f
simon <simon@pop-os.localdomain>
2019-04-11 18:14:59 +0200

    Added types for timetable api

---
+++ server/interfaces/Period.ts
@@ -0,0 +1,10 @@
+export interface Period {
+  startHour: number
+  endHour: number
+  duration: number
+  date: Date
+  subjectShort: string
+  subjectLong: string
+  roomShort: string
+  roomLong: string
+}
