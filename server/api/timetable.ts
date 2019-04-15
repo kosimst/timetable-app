104d020c2d759f7261fd1a152720f6253dfe8b8c
simon <simon@pop-os.localdomain>
2019-04-15 10:59:31 +0200

    Fixed date bug

--- server/api/timetable.ts
+++ server/api/timetable.ts
@@ -8,7 +8,7 @@ require('events').EventEmitter.prototype._maxListeners = 100
 
 router.get('/source/:source', async function(req, res) {
   const source = req.params.source
-  const date = new Date(req.query.date || Date.now())
+  const date = new Date(parseInt(req.query.date) || Date.now())
 
   res.send(await fetchSource(source, date))
 })
