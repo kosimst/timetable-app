12763fb5f770e5a5c4a1298a9effdb761b1fd09a
simon <simon@pop-os.localdomain>
2019-06-11 17:42:05 +0200

    Remove prpl-server

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 // @ts-ignore
 import secure from 'express-force-https'
-const prpl = require('prpl-server')
 const rendertron = require('rendertron-middleware')
+const path = require('path')
 
 const app = express()
 
 /* Force https */
 app.use(secure)
+
+/* Use static */
+app.use(express.static(path.join(__dirname, 'build')))
 
 /* Rendertron proxy for bots */
 app.use(
@@ -16,15 +19,12 @@ app.use(
   }),
 )
 
+/* SPA */
 app.get(
   '/*',
-  prpl.makeHandler('./build', {
-    builds: [
-      {
-        name: 'default',
-      }
-    ],
-  }),
+  (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/default/index.html'))
+  }
 )
 
 app.listen(process.env.PORT || 7000)
